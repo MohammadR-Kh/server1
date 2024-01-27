6a91e5097778b1dac87e188aaa33b84fe70caaa0
Mohammad111300 <mammadelec@gmail.com>
2024-01-27 14:22:56 +0330

    first commit

---
+++ models/Posts.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Posts = sequelize.define("Posts", {
+        title: {
+            type: DataTypes.STRING,
+            allowNULL: false,
+        },
+        postText: {
+            type: DataTypes.STRING,
+            allowNULL: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNULL: false,
+        },
+
+    });
+
+    Posts.associate = (models) => {
+        Posts.hasMany(models.Comments, {
+            onDelete: "cascade",
+        });
+        Posts.hasMany(models.Likes, {
+            onDelete: "cascade",
+        });
+    };
+
+    return Posts;
+};
---
+++ models/Users.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Users = sequelize.define("Users", {
+        username: {
+            type: DataTypes.STRING,
+            allowNULL: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNULL: false,  
+        }
+    });
+
+    Users.associate = (models) => {
+        Users.hasMany(models.Likes, {
+            onDelete: "cascade",
+        });
+        Users.hasMany(models.Posts, {
+            onDelete: "cascade",
+        });
+    };
+
+    return Users;
+};
